70e097725d1ee526b483e60fd68ff6984d9bf616
Maneren <49210777+Maneren@users.noreply.github.com>
2022-07-15 18:32:05 +0200

    refactor all and add more options

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,61 @@
 use std::time::Instant;
 
 macro_rules! timeit {
-  ($a:block,$b:expr) => {
+  ($a:expr,$b:expr) => {
     let start = Instant::now();
 
-    $a
+    let _ = $a;
 
     let elapsed = start.elapsed();
     println!("{} in {elapsed:?}", $b);
   };
 }
 
+fn isqrt(n: usize) -> usize {
+  (n as f64).sqrt() as usize
+}
+
 fn main() {
   // check it if is working correctly
   // 7919 is taken from wikipedia
   assert_eq!(primes_iter().nth(999), Some(7919));
-  assert_eq!(primes_sieve(1_000).get(999), Some(&7919_usize));
+  assert_eq!(primes_sieve(1_000).get(999), Some(&7919));
+  assert_eq!(primes_sieve_iter().nth(999), Some(7919));
+  assert_eq!(PrimeSieve::new().nth(999), Some(7919));
+
+  println!("Values correct, timing...\n");
+
+  const COUNT: usize = 2_000_000;
+
+//  assert!(is_x86_feature_detected!("avx2"));
 
   timeit!(
-    {
-      primes_iter().take(10_000).for_each(drop);
-    },
-    "10k primes (iter)"
+    primes_iter().take(COUNT).collect::<Vec<_>>(),
+    "2M primes (iter::collect)"
   );
 
   timeit!(
-    {
-      primes_iter().nth(9999);
-    },
-    "10k-th prime (iter)"
+    primes_sieve_iter().take(COUNT).collect::<Vec<_>>(),
+    "2M primes (iter_sieve::collect)"
   );
 
   timeit!(
-    {
-      primes_sieve(10_000);
-    },
-    "10k primes (sieve)"
+    PrimeSieve::new().take(COUNT).collect::<Vec<_>>(),
+    "2M primes (PrimeSieve::collect)"
   );
+
+  timeit!(primes_sieve(COUNT), "2M primes (pure sieve)");
 }
 
-fn is_prime(n: &usize) -> bool {
-  if *n <= 3 {
-    return *n > 1;
-  }
-
-  if n % 2 == 0 || n % 3 == 0 {
-    return false;
-  }
-
-  let upper_bound = ((*n as f64).sqrt().ceil() + 1.0) as usize;
-
-  (5_usize..upper_bound)
-    .step_by(6)
-    .all(|i| n % i != 0 && n % (i + 2) != 0)
-}
 fn primes_iter() -> impl Iterator<Item = usize> {
-  (2..).filter(is_prime)
+  (2..).filter(|&n| match n {
+    0 | 1 => false,
+    2 | 3 => true,
+    n if n % 2 == 0 || n % 3 == 0 => false,
+    _ => (5_usize..(isqrt(n) + 1))
+      .step_by(6)
+      .all(|i| n % i != 0 && n % (i + 2) != 0),
+  })
 }
 
 fn primes_sieve(count: usize) -> Vec<usize> {
@@ -64,7 +64,13 @@ fn primes_sieve(count: usize) -> Vec<usize> {
   let mut n = 2;
 
   'outer: while primes.len() < count {
+    let upper_bound = isqrt(n) + 1;
+
     for i in &primes {
+      if *i > upper_bound {
+        break;
+      }
+
       if n % i == 0 {
         n += 1;
         continue 'outer;
@@ -77,4 +83,69 @@ fn primes_sieve(count: usize) -> Vec<usize> {
   }
 
   primes
+}
+
+struct PrimeSieve {
+  primes: Vec<usize>,
+}
+
+impl PrimeSieve {
+  pub fn new() -> PrimeSieve {
+    PrimeSieve { primes: Vec::new() }
+  }
+}
+
+impl Iterator for PrimeSieve {
+  type Item = usize;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.primes.is_empty() {
+      self.primes.push(2);
+      return Some(2);
+    }
+
+    let mut n = self.primes.last()? + 1;
+
+    let next = loop {
+      let upper_bound = isqrt(n) + 1;
+
+      if self
+        .primes
+        .iter()
+        .take_while(|i| **i <= upper_bound)
+        .all(|i| n % i != 0)
+      {
+        break n;
+      }
+
+      n += 1
+    };
+
+    self.primes.push(next);
+    Some(next)
+  }
+}
+
+fn primes_sieve_iter() -> impl Iterator<Item = usize> {
+  (1..).scan(Vec::new(), |primes, i| {
+    let mut n = *primes.last().unwrap_or(&i) + 1;
+
+    loop {
+      let upper_bound = isqrt(n) + 1;
+
+      if primes
+        .iter()
+        .take_while(|i| **i < upper_bound)
+        .all(|i| n % i != 0)
+      {
+        break;
+      }
+
+      n += 1;
+    }
+
+    primes.push(n);
+
+    Some(n)
+  })
 }
